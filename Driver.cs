31f737fd5efa4aa5f21605ce789755c4e634b6c7
Dylan <leibeldms@gmail.com>
2021-07-12 17:52:45 -0700

    Add files via upload

---
+++ Driver.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NvoicepayFactorialAutomation.Utilities
+{
+    public class Driver
+    {
+        public static IWebDriver Instance { get; set; }
+
+        public static void Initialize()
+        {
+            Instance = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            Instance.Manage().Window.Maximize();
+            Instance.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        public static void Quit()
+        {
+            Instance.Quit();
+        }
+
+        internal static object FindElement(By by)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Initialize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NvoicepayFactorialAutomation.Utilities
+{
+    public class Initialize
+    {
+        public static void GoTo()
+        {
+            Utilities.Driver.Instance.Navigate().GoToUrl("http://qainterview.pythonanywhere.com/");
+        }
+    }
+}
